03d5116c0ad2c7d0561bc966a6fe882be1beb90d
IrhamIzza <Irhamizza21@gmail.com>
2026-02-01 20:48:14 +0700

    update testimonial

--- src/components/TestimonialCarousel.jsx
+++ src/components/TestimonialCarousel.jsx
@@ -31,7 +31,7 @@ const testimonials = [
 export default function TestimonialSection() {
   return (
     <section className="">
-      <div className="">
+      <div className="home-page">
         {/* LEFT CONTENT */}
         <Swiper
           spaceBetween={30}
@@ -49,16 +49,14 @@ export default function TestimonialSection() {
         >
           {testimonials.map((item, index) => (
             <SwiperSlide key={index}>
-              <div className="flex rounded-xl p-8 py-12">
+              <div className="flex rounded-xl px-2 py-2 pb-12">
                 <img
                   src={item.img}
                   alt=""
-                  className="w-full h-full max-w-[70%] object-cover shadow-[0_0_10px_rgba(0,0,0,0.9)] rounded-lg"
+                  className="w-full h-full max-w-[70%] object-cover shadow-[0_0_10px_rgba(0,0,0,0.6)] rounded-lg border-2 border-white"
                 />
-                <div className="absolute max-h-[50%] bottom-2 left-[40%] mr-2 bg-[#FFF6E5] py-6 px-4 rounded-xl shadow-[0_0_10px_rgba(0,0,0,0.9)] overflow-visible">
-                  <p className="text-gray-700 leading-relaxed text-sm mb-2">
-                    {item.text}
-                  </p>
+                <div className="absolute max-h-[50%] bottom-2 left-[40%] mr-2 bg-[#FFF5D2] py-6 px-4 rounded-xl shadow-[0_0_10px_rgba(0,0,0,0.6)] overflow-visible">
+                  <p className=" leading-relaxed text-sm mb-2">{item.text}</p>
                   <p className="font-semibold text-gray-900 text-center">
                     {item.name}
                   </p>
@@ -67,34 +65,6 @@ export default function TestimonialSection() {
             </SwiperSlide>
           ))}
         </Swiper>
-
-        {/* RIGHT CAROUSEL */}
-        {/* <Swiper
-          modules={[Pagination, Autoplay]}
-          slidesPerView={1}
-          spaceBetween={30}
-          pagination={{ clickable: true }}
-          autoplay={{ delay: 4000 }}
-          className="relative"
-        >
-          {testimonials.map((item, index) => (
-            <SwiperSlide key={index}>
-              <div className="relative bg-[#FFF6E5] rounded-xl p-8 shadow-lg">
-              
-                <span className="absolute -top-6 right-6 text-6xl text-[#E8D9B5] font-serif">
-                  “
-                </span>
-
-                <p className="text-gray-700 leading-relaxed mb-6">
-                  {item.text}
-                </p>
-                <span className="font-semibold text-gray-900">
-                  {item.name}
-                </span>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper> */}
       </div>
     </section>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,65 +3,44 @@ import TestimonialCarousel from "../components/TestimonialCarousel";
 
 export default function Home() {
   return (
-    <div className=" py-10 px-32 flex flex-col gap-20">
+    <div className=" pt-10 pb-4 px-32 flex flex-col gap-20">
       {/* Landing Page */}
       <div className="flex  items-center gap-20">
         <div className="flex-1 flex flex-col gap-2 ">
           <h1 className="text-xl font-bold">Halo, Kami Adalah</h1>
           <h1 className="text-4xl font-bold ">Interior Designer</h1>
           <h3 className="text-xl font-bold ">
-            Kami Menyediakan{" "}
-            <span className="text-gold">Berbagai Macam Desain</span>{" "}
+            Kami Menyediakan <span className="text-gold">Berbagai Macam Desain</span>{" "}
           </h3>
 
           <p className="mt-10 max-w-[460px]">
-            Kami ingin membuat{" "}
-            <span className="font-bold">Website Interior Design</span> menjadi
-            forum yang dapat menjadi jembatan antara jasa desain inerior dan
-            peminat desain
+            Kami ingin membuat <span className="font-bold">Website Interior Design</span> menjadi forum yang dapat
+            menjadi jembatan antara jasa desain inerior dan peminat desain
           </p>
         </div>
 
         {/* IMAGE GRID */}
         <div className="grid grid-cols-2 grid-rows-2 gap-5 h-[400px] flex-1">
           {/* BESAR */}
-          <img
-            src="/img/Head (1).jpg"
-            className="row-span-2 w-full h-full object-cover rounded-lg"
-            alt=""
-          />
+          <img src="/img/Head (1).jpg" className="row-span-2 w-full h-full object-cover rounded-lg" alt="" />
           {/* KECIL ATAS */}
-          <img
-            src="/img/Head (2).jpg"
-            className="w-full h-full object-cover rounded-lg"
-            alt=""
-          />
+          <img src="/img/Head (2).jpg" className="w-full h-full object-cover rounded-lg" alt="" />
           {/* KECIL BAWAH */}
-          <img
-            src="/img/Head (3).jpg"
-            className="w-full h-full object-cover rounded-lg"
-            alt=""
-          />
+          <img src="/img/Head (3).jpg" className="w-full h-full object-cover rounded-lg" alt="" />
         </div>
       </div>
       {/* Tentang Kami */}
       <div className="flex gap-24 items-center">
-        <img
-          src="/img/logo.jpg"
-          className="max-w-[300px] rounded-full border-2 object-cover"
-        />
+        <img src="/img/logo.jpg" className="max-w-[300px] rounded-full border-2 object-cover" />
         <div className="flex flex-col gap-12">
           <h1 className="text-3xl font-bold">
             <span className="text-gold">Tentang </span> Kami
           </h1>
           <p>
-            Kami adalah studio desain interior yang berfokus pada gaya tropis,
-            menghadirkan suasana hangat, alami, dan menyegarkan ke dalam setiap
-            ruang. Dengan memadukan elemen alam, pencahayaan yang harmonis,
-            serta palet warna lembut, kami menciptakan interior yang tidak hanya
-            indah, tetapi juga nyaman dan menenangkan. Kami percaya bahwa hunian
-            yang baik adalah ruang yang selaras dengan lingkungan dan gaya hidup
-            penghuninya.
+            Kami adalah studio desain interior yang berfokus pada gaya tropis, menghadirkan suasana hangat, alami, dan
+            menyegarkan ke dalam setiap ruang. Dengan memadukan elemen alam, pencahayaan yang harmonis, serta palet
+            warna lembut, kami menciptakan interior yang tidak hanya indah, tetapi juga nyaman dan menenangkan. Kami
+            percaya bahwa hunian yang baik adalah ruang yang selaras dengan lingkungan dan gaya hidup penghuninya.
           </p>
         </div>
       </div>
@@ -74,32 +53,25 @@ export default function Home() {
           <div className="overflow-hidden rounded-md shadow-[0_0_10px_rgba(0,0,0,0.9)]">
             <img src="/img/Kategori1.jpg" alt="" />
             <div className="bg-gold p-2">
-              <p className="font-medium text-lg text-white text-center">
-                Public Places
-              </p>
+              <p className="font-medium text-lg text-white text-center">Public Places</p>
             </div>
           </div>
           <div className="overflow-hidden rounded-md shadow-[0_0_10px_rgba(0,0,0,0.9)]">
             <img src="/img/Kategori2.jpg" alt="" />
             <div className="bg-gold p-2">
-              <p className="font-medium text-lg text-white text-center">
-                Private Places
-              </p>
+              <p className="font-medium text-lg text-white text-center">Private Places</p>
             </div>
           </div>
         </div>
       </div>
       {/* Testimoni */}
-      <div className="flex flex-col gap-2 px-16">
+      <div className="flex flex-col gap-6 md:px-48">
         <h1 className="text-3xl font-bold text-center">
           <span className="text-gold">Testimoni</span> pengguna
         </h1>
-        {/* section lain */}
-
         <TestimonialCarousel />
-
-        {/* section lain */}
       </div>
+
     </div>
   );
 }
