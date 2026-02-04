7df777826c99a18ece8b662da5ace6c31298842e
IrhamIzza <Irhamizza21@gmail.com>
2026-02-04 10:28:25 +0700

    add search

--- src/pages/Kategori.jsx
+++ src/pages/Kategori.jsx
@@ -5,6 +5,8 @@ import { Input } from "@/components/ui/input";
 
 export default function Kategori() {
   const [search, setSearch] = useState("");
+  const [selectedCard, setSelectedCard] = useState(null);
+
   const menuDesain = [
     "Minimalis",
     "Skandinavia",
@@ -44,7 +46,7 @@ export default function Kategori() {
   const filteredCard = card.filter((item) =>
     item.title.toLowerCase().includes(search.toLowerCase()),
   );
-  
+
   return (
     <div className="flex flex-col gap-10 py-10 px-20">
       <h1 className="text-3xl font-bold text-center">
@@ -55,20 +57,73 @@ export default function Kategori() {
         <Dropdown nama="Jenis Desain Interior" items={menuDesain}></Dropdown>
         <Dropdown nama="Jenis Bangunan" items={menujenisBangunan}></Dropdown>
         {/* Search */}
-        <Input
-          type="text"
-          placeholder="Cari kategori"
-          className="w-full"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
+        <div className="relative w-full">
+          <i className="ph ph-magnifying-glass absolute left-3 top-1/2 -translate-y-1/2  text-lg"></i>
+
+          <Input
+            type="text"
+            placeholder="Cari kategori"
+            className="w-full pl-10"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
       </div>
       {/* Card */}
-      <div className="grid grid-cols-3 px-20   gap-10 mx-auto">
+      <div className="grid grid-cols-3 px-20 gap-10 mx-auto">
         {filteredCard.map((item, index) => (
-          <Card key={index} title={item.title} img={item.img} />
+          <div
+            key={index}
+            onClick={() => setSelectedCard(item)}
+            className="cursor-pointer"
+          >
+            <Card title={item.title} img={item.img} />
+          </div>
         ))}
       </div>
+      {selectedCard && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+          {/* Overlay */}
+          <div
+            className="absolute inset-0 bg-black/50"
+            onClick={() => setSelectedCard(null)}
+          ></div>
+
+          {/* Modal box */}
+          <div
+            className="relative bg-white rounded-xl p-6 w-[40vw] shadow-lg z-10              
+              animate-jump-in animate-delay-200 animate-once"
+          >
+            <div>
+              <h2 className="text-lg font-bold text-center">
+                {selectedCard.title}
+              </h2>
+              <p className="text-lg font-bold text-center mb-4">Kota Blitar</p>
+            </div>
+
+            <div className="rounded-lg overflow-hidden mb-4">
+              <img
+                src={selectedCard.img}
+                alt={selectedCard.title}
+                className="w-full object-cover"
+              />
+            </div>
+
+            <div className="flex justify-between">
+              <button
+                onClick={() => setSelectedCard(null)}
+                className="px-4 py-2 border rounded-md text-sm cursor-pointer"
+              >
+                Kembali
+              </button>
+
+              <button className="px-4 py-2 bg-black text-white rounded-md text-sm cursor-pointer">
+                Lihat Hasil Konstruksi
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
