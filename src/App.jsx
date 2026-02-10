4b0c64330119f1c1f95833f9ef0470c4cc3b9dbf
IrhamIzza <Irhamizza21@gmail.com>
2026-02-11 01:47:04 +0700

    add toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import Navbar from "./components/Navbar";
+import { Toaster } from "@/components/ui/sonner";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Kategori from "./pages/Kategori";
 import Pemesanan from "./pages/Pemesanan";
 import DetailPemesanan from "./pages/DetailPemesanan";
 import Pembayaran from "./pages/Pembayaran";
+import Pembayaran2 from "./pages/Pembayaran2";
+import Pembayaran3 from "./pages/Pembayaran3";
+import ScrollTop from "./components/ScrollTop";
 
 function App() {
   return (
     <>
     <Navbar>
     </Navbar>
+        <ScrollTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/kategori" element={<Kategori />} />
         <Route path="/pemesanan" element={<Pemesanan />} />
         <Route path="/pemesanan/detail_pemesanan" element={<DetailPemesanan />} />
         <Route path="/pemesanan/pembayaran" element={<Pembayaran />} />
+        <Route path="/pemesanan/pembayaran2" element={<Pembayaran2 />} />
+        <Route path="/pemesanan/pembayaran3" element={<Pembayaran3 />} />
       </Routes>
+      <Toaster />
     </>
   );
 }
---
+++ src/components/ui/sonner.jsx
@@ -0,0 +1,40 @@
+import {
+  CircleCheckIcon,
+  InfoIcon,
+  Loader2Icon,
+  OctagonXIcon,
+  TriangleAlertIcon,
+} from "lucide-react"
+import { useTheme } from "next-themes"
+import { Toaster as Sonner } from "sonner";
+
+const Toaster = ({
+  ...props
+}) => {
+  const { theme = "system" } = useTheme()
+
+  return (
+    <Sonner
+    position= "top-center"
+      theme={theme}
+      className="toaster group"
+      icons={{
+        success: <CircleCheckIcon className="size-4" />,
+        info: <InfoIcon className="size-4" />,
+        warning: <TriangleAlertIcon className="size-4" />,
+        error: <OctagonXIcon className="size-4" />,
+        loading: <Loader2Icon className="size-4 animate-spin" />,
+      }}
+      style={
+        {
+          "--normal-bg": "#000000",
+          "--normal-text": "#ffffff",
+          "--normal-border": "#1f1f1f",
+          "--border-radius": "12px"
+        }
+      }
+      {...props} />
+  );
+}
+
+export { Toaster }
